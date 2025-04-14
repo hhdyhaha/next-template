0daca46e6b06bd687db21aced07c3f16e72d8dfd
hhdyhaha <1164549585@qq.com>
2025-04-14 14:15:29 +0800

    feat: 移除首页组件中的未使用的 Image 导入，并优化主题切换组件的主题切换逻辑

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { useEffect } from "react";
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import Counter from "@/components/common/Counter";
